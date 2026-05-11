bdab1163fc670e93da74954015fc6cf7f61c4edc
FranzoniLeo <franzoni.leoalves@gmail.com>
2026-05-11 11:40:24 -0300

    questao 2 funcionando

---
+++ trabalho_1/Q2/utils/gauss.py
@@ -0,0 +1,40 @@
+import numpy as np
+
+'''
+Limitações:
+Sem pivoteamento: se A[i, i] == 0 (ou quase zero), há divisão por zero (ou instabilidade). Algoritmos robustos trocam linhas (pivoteamento parcial).
+'''
+
+class Gauss:
+    def __init__(self, A, b):
+        self.A = np.array(A, dtype=float)
+        self.b = np.array(b, dtype=float)
+
+    def triangularizacao(self):
+        n = len(self.A)
+
+        for i in range(n): #colunas
+            for j in range(i + 1, n):#linhas
+                pivot = self.A[j, i]
+                diagonal = self.A[i, i]
+                #print(pivot, diagonal)
+                factor = pivot / diagonal 
+                for k in range(i, n):
+                    self.A[j, k] -= factor * self.A[i, k] #aplicar o  fator a linha j
+                self.b[j] -= factor * self.b[i] #aplicar o fator ao b
+
+    def retrosubstituicao(self):
+        n = len(self.A)
+        x = np.zeros(n)
+        for i in range(n - 1, -1, -1):
+            soma = 0.0
+            for j in range(i + 1, n):
+                soma += self.A[i, j] * x[j]
+            # x[i] = (b[i] - soma) / A[i,i]
+            x[i] = (self.b[i] - soma) / self.A[i, i]
+        self.x = x
+        return self.x
+
+    def imprimir(self):
+        print(" ")
+        print(self.x)
