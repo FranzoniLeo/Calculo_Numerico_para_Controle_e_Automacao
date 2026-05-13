69f0f0abd0d05edd578d161f23d347eb08b1276a
FranzoniLeo <franzoni.leoalves@gmail.com>
2026-05-12 23:18:06 -0300

    correção criterio de parada Q2 metodo de Jacobi

--- trabalho_1/Q2/main.py
+++ trabalho_1/Q2/main.py
@@ -15,6 +15,7 @@ A = [[4, -1, 0, -1, 0, 0, 0, 0, 0],
 
 b = [175, 100, 150, 75, 0, 50, 75, 0, 50]
 
+
 gauss = Gauss(A, b)
 gauss.triangularizacao()
 gauss.retrosubstituicao()
@@ -24,6 +25,7 @@ lup = LUP(A, b)
 lup.fatoracaoLUP()
 lup.resolver()
 lup.imprimir()
+
 
 jacobi = Jacobi(A, b)
 jacobi.resolver(x_0 = [0, 0, 0, 0, 0, 0, 0, 0, 0])
--- trabalho_1/Q2/utils/Jacobi.py
+++ trabalho_1/Q2/utils/Jacobi.py
@@ -6,9 +6,13 @@ class Jacobi:
         self.b = np.array(b, dtype=float)
 
     def convergencia(self, X_k1, X_k):
-        return np.linalg.norm(X_k1 - X_k, ord=np.inf) / np.linalg.norm(X_k, ord=np.inf)
+        num = np.linalg.norm(X_k1 - X_k, ord=np.inf)
+        den = np.linalg.norm(X_k1, ord=np.inf)
+        if den == 0:
+            return 0.0 if num == 0 else np.inf
+        return num / den
 
-    def resolver(self, x_0 = None, max_iters = 100, p = 1e-6):
+    def resolver(self, x_0=None, max_iters=100, p=1e-6):
         A = self.A
         b = self.b
         n = len(A)
@@ -19,17 +23,26 @@ class Jacobi:
         for j in range(n):
             x_k[j] = (1/A[j][j]) * (b[j] - np.sum(A[j][:j] * x_0[:j]) - np.sum(A[j][j+1:] * x_0[j+1:]))
 
-        while k < max_iters:
+        while (k < max_iters):
             for j in range(n):
 
                 fator = 1/A[j, j]
                 x_k1[j] = fator * (b[j] - np.sum(A[j][:j] * x_k[:j]) - np.sum(A[j][j+1:] * x_k[j+1:]))
 
+            if self.convergencia(x_k1, x_k) < p:
+                x_k = x_k1.copy()
+                k = k + 1
+                break
+
+            x_k = x_k1.copy()
             k = k + 1
-            x_k = x_k1
+
+
+        #print(k)
 
         self.x_k = x_k
         return self.x_k
 
     def imprimir(self):
+        print(" ")
         print(self.x_k)
