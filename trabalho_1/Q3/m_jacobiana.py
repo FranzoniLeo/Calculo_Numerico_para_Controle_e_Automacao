37e3585838aa48b62caed45d03c5e788316ecd1c
FranzoniLeo <franzoni.leoalves@gmail.com>
2026-05-13 01:01:12 -0300

    questao 3

---
+++ trabalho_1/Q3/m_jacobiana.py
@@ -0,0 +1,89 @@
+import numpy as np
+import sympy as sp
+
+
+class Jacobiana:
+    def __init__(self, Fq, Q, h = 1e-6):
+        self.Fq = Fq
+        self.Q = np.array(Q, dtype=float)
+        self.h = h
+        self.determinar()
+        self.J = self.calcular()
+
+
+    def determinar(self):
+        Q1, Q2, Q3, Q4, Q5, Q6 = sp.symbols('Q1 Q2 Q3 Q4 Q5 Q6')
+        Q = sp.Matrix([Q1,Q2,Q3,Q4,Q5,Q6])
+
+        F = sp.Matrix([
+            Q1 - 10,
+            Q1 - Q2 - Q3,
+            Q2 - Q4 - Q6,
+            Q3 + Q4 - Q5,
+            Q2**2 + Q4**2 - 4*Q3**2,
+            Q4**2 + Q5**2 - 4*Q6**2
+        ])
+
+        J = F.jacobian(Q)
+        sp.pprint(J)
+
+
+    def calcular(self):
+        Fx = self.Fx
+        h = self.h
+
+        J = np.zeros((len(Fx(Q)), len(Q)))
+
+        for i in range(len(Q)):
+            dx = np.zeros(len(Q))
+            dx[i] = h
+            J[:, i] = (Fx(Q + dx) - Fx(Q)) / h
+
+        self.J = J
+        return self.J
+        
+
+    def Fx(self, Q):
+        Q1, Q2, Q3, Q4, Q5, Q6 = Q
+
+        return np.array([
+            Q1 - 10,
+            Q1 - Q2 - Q3,
+            Q2 - Q4 - Q6,
+            Q3 + Q4 - Q5,
+            Q2**2 + Q4**2 - 4*Q3**2,
+            Q4**2 + Q5**2 - 4*Q6**2
+        ])
+
+    def convergencia(self, Xk, Xk1):
+        return np.linalg.norm(Xk1 - Xk, ord=np.inf)
+
+    def resolver(self, max_iters = 100, p = 1e-8):
+        Fx = self.Fx
+
+        Xk = self.Q.copy()
+        Xk1 = Xk + 2 * p
+        k = 0
+
+        while (k < max_iters) and (self.convergencia(Xk, Xk1) > p):
+            if k > 0:
+                Xk = Xk1
+
+            J = self.calcular(Xk)
+
+            delta = np.linalg.solve(J, Fx(Xk))
+            Xk1 = Xk - delta
+
+            k = k + 1
+        
+        print(k)
+
+        self.x_k1 = Xk1
+        return self.x_k1
+
+
+    def imprimir(self):
+        print(" ")
+        print(self.x_k1)
+
+        print(self.Fx(self.x_k1))
---
+++ trabalho_1/Q3/main.py
@@ -0,0 +1,15 @@
+from m_jacobiana import Jacobiana
+
+
+Q = [10, 5, 5, 2, 7, 3]
+
+Fq = [[Q[0] - 10],
+      [Q[0] - Q[1] - Q[2]],
+      [Q[1] - Q[3] - Q[5]],
+      [Q[2] + Q[3] - Q[4]],
+      [Q[1]**2 + Q[3]**2 - 4*Q[2]**2],
+      [Q[3]**2 + Q[4]**2 - 4*Q[5]**2]]
+
+jacobi = Jacobiana(Fq, Q)
+jacobi.resolver()
+jacobi.imprimir()
